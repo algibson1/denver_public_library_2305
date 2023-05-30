bea9abc799d35ad5cf4fc6e4ba143798e9317e38
Artemy Gibson <artemygibson@gmail.com>
2023-05-29 17:34:51 -0700

    Create author class

---
+++ lib/author.rb
@@ -0,0 +1,18 @@
+class Author
+  attr_reader :books
+  def initialize(author_name)
+    @first_name = author_name[:first_name]
+    @last_name = author_name[:last_name]
+    @books = []
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def write(title, publication_date)
+    new_book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
+    @books << new_book
+    new_book
+  end
+end
