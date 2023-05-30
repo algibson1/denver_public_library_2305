cf4419443df3bbcedf87e48f794c0269a6480616
Artemy Gibson <artemygibson@gmail.com>
2023-05-29 18:17:42 -0700

    Add checkout and return methods

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,13 @@
 class Book
-  attr_reader :title
+  attr_reader :title,
+              :times_checked_out
   def initialize(book_details)
     @author_first_name = book_details[:author_first_name]
     @author_last_name = book_details[:author_last_name]
     @title = book_details[:title]
     @publication_date = book_details[:publication_date]
+    @checked_out_status = false
+    @times_checked_out = 0
   end
 
   def author
@@ -13,5 +16,16 @@ class Book
 
   def publication_year
     @publication_date[-4..-1]
+  end
+
+  def checkout
+    return false if @checked_out_status == true
+    @times_checked_out += 1
+    @checked_out_status = true
+  end
+  
+  def return
+    return "Not checked out" if @checked_out_status == false
+    @checked_out_status = false
   end
 end 
