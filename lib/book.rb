62389b06cc3a7be41aae5252e53e43f49a881eea
Artemy Gibson <artemygibson@gmail.com>
2023-05-29 17:25:04 -0700

    Create book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 class Book
+  attr_reader :title
+  def initialize(book_details)
+    @author_first_name = book_details[:author_first_name]
+    @author_last_name = book_details[:author_last_name]
+    @title = book_details[:title]
+    @publication_date = book_details[:publication_date]
+  end
+
+  def author
+    "#{@author_first_name} #{@author_last_name}"
+  end
+
+  def publication_year
+    @publication_date[-4..-1]
+  end
 end 
