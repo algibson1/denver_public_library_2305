c1df08c1e2547c47027753966f007a17d2aec4d2
Artemy Gibson <artemygibson@gmail.com>
2023-05-29 18:41:35 -0700

    Add return, publication time frame, and checkout methods

--- lib/library.rb
+++ lib/library.rb
@@ -1,16 +1,37 @@
 class Library
   attr_reader :name,
               :books,
-              :authors
+              :authors,
+              :checked_out_books
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
     @authors << author
     @books << author.books
     @books.flatten!
+  end
+
+  def publication_time_frame(author)
+    return "Author not found" if !authors.include?(author)
+    book_years = author.books.map {|book| book.publication_year}
+    ordered_years = book_years.sort 
+    {start: ordered_years.first, end: ordered_years.last}
+  end
+
+  def checkout(book)
+    return false if !@books.include?(book) || book.checked_out?
+    @checked_out_books << book
+    book.checkout
+  end
+
+  def return(book)
+    return "#{book.title} is not checked out" if !book.checked_out?
+    @checked_out_books.delete(book)
+    book.return
   end
 end
